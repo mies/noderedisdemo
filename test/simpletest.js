c3d9a060ce86918a30846e32d2a81c166635f2c7
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2012-07-20 13:24:30 -0700

    started unit test

---
+++ test/simpletest.js
@@ -0,0 +1,13 @@
+var redis = require("redis");
+
+var client = redis.createClient(6379, process.env.WERCKER_REDIS_IPADDRESS)
+
+exports.testSets = function(test) {
+  client.sadd("some set", "mem 1");
+  client.sadd("some set", "mem 2");
+  client.sadd("some set", "mem 3");
+  client.sadd("some set", "mem 4");
+  client.smembers("missing set", function(err, reply) {
+    console.log(reply);
+  });
+};
